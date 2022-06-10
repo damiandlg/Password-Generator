1b5bac3aeeabb59ceee8932cf91d717c268d9d8e
damiandlg <damian.delagarzaf@gmail.com>
2022-06-10 19:35:38 -0400

    menu options in pop up

--- Develop/script.js
+++ Develop/script.js
@@ -13,27 +13,25 @@ const appState = {
 
 }
 
-/**
- * Modal
- */
- const myModal = document.getElementById('myModal')
- const myInput = document.getElementById('myInput')
- 
-//  myModal.addEventListener('shown.bs.modal', () => {
-//    myInput.focus()
-//  })
+?
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+  passwordText.value = password;
 
-// }
+}
+
+function generatePassword(){
+  return "New Password"
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
