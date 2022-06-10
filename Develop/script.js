88993c0eb3cff0344dbbe1970bc7ab7b565f9689
damiandlg <damian.delagarzaf@gmail.com>
2022-06-10 18:56:14 -0400

    Button with some variables working

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,39 @@
 // Assignment code here
+/**
+ * App State
+ */
+const appState = {
+  passwordLength: 8,
+  types: {
+    lowercase: false,
+    uppercase: false,
+    numeric: false,
+    specialCharacter: false
+  },
 
+}
+
+/**
+ * Modal
+ */
+ const myModal = document.getElementById('myModal')
+ const myInput = document.getElementById('myInput')
+ 
+//  myModal.addEventListener('shown.bs.modal', () => {
+//    myInput.focus()
+//  })
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
